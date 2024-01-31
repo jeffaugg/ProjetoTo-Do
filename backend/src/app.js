218eba46a4ad661e7932528ff0d92619d98a3dc5
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-01-31 17:45:09 -0300

    added  cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = require('./router');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json()); // está dizendo que o app vai usar o express.json sempre que receber uma requisição
 app.use(router); // está dizendo que o app vai usar o router sempre
 
